d26151d502f78ecfce5c9f1b9881042f53f6d3a8
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2019-05-31 09:38:55 +0200

    basic implementation

--- atoms.cpp
+++ atoms.cpp
@@ -2,4 +2,47 @@
 // Created by hossein on 5/30/19.
 //
 
-#include "Atoms.h"
+#include "atoms.h"
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+/* ----------------------------------------------------------------------
+   setup for Atom
+------------------------------------------------------------------------- */
+
+Atom::Atom(double x, double y, double z, std::string element): x(x), y(y), z(z), element(element) {}
+
+
+/* ----------------------------------------------------------------------
+   setup for Atoms
+------------------------------------------------------------------------- */
+
+void Atoms::read_xyz(std::string filename)
+{
+    std::ifstream inFile(filename);
+    if (!inFile) {
+        std::cerr << "Unable to open " << filename;
+        exit(1);   // call system to stop
+    }
+
+    // TODO: need improvement
+    /* read number fo atoms */
+    int nAtoms;
+    inFile >> nAtoms;
+    /* skip the second line */
+    std::string line;
+    inFile >> line;
+    /* read atomic names and coordinates */
+    for( int nline=0; nline<nAtoms; nline++)
+    {
+        std::getline(inFile, line);
+        std::stringstream ss(line);
+
+        double x, y, z;
+        std::string element;
+        ss >> element >> x >> y >> z;
+        atoms.push_back( Atom(x, y, z, element) );
+    }
+    inFile.close();
+}
--- atoms.h
+++ atoms.h
@@ -5,12 +5,23 @@
 #ifndef NNP_ATOMS_H
 #define NNP_ATOMS_H
 
+#include <string>
+#include <vector>
+
+
+class Atom {
+public:
+    double x, y, z;
+    std::string element;
+    Atom(double x, double y, double z, std::string element);
+};
 
 
 class Atoms {
-
+public:
+    std::vector<Atom> atoms;
+    void read_xyz(std::string filename);
 };
-
 
 
 #endif //NNP_ATOMS_H
