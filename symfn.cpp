ed8f230b679e2363b5988dd0d10033efac49ad23
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2019-05-31 12:33:07 +0200

    primary implementation of symmetric function factory design

--- symfn.cpp
+++ symfn.cpp
@@ -5,12 +5,17 @@
 #include <cmath>
 #include "symfn.h"
 
-# define M_PI   3.14159265358979323846  /* pi */
-
+//using namespace NNP_SF;
 
 /* ----------------------------------------------------------------------
-   setup for base symmetric function
+   setup for base symmetric function base
 ------------------------------------------------------------------------- */
+
+SymmetricFunction::SymmetricFunction(double r_cutoff): r_cutoff(r_cutoff) {}
+
+double SymmetricFunction::function(double r) { return 0; }
+
+double SymmetricFunction::calculate() { return 0; }
 
 double SymmetricFunction::fn_cutoff(double r)
 {
@@ -21,28 +26,31 @@ double SymmetricFunction::fn_cutoff(double r)
    setup for G0 symmetric function
 ------------------------------------------------------------------------- */
 
-double G0::calculate()
-{
+G0::G0(double r_cutoff): SymmetricFunction(r_cutoff) {}
 
-}
+double G0::function(double r) { return fn_cutoff(r); }
+
+double G0::calculate() { return 0; }
 
 /* ----------------------------------------------------------------------
    setup for G1 symmetric function
 ------------------------------------------------------------------------- */
+
+G1::G1(double r_cutoff, double eta, double rs): SymmetricFunction(r_cutoff), eta(eta), rs(rs) {}
 
 double G1::function(double rij)
 {
     return exp( -eta * (rij-rs) * (rij-rs) ) * fn_cutoff(rij);
 }
 
-double G1::calculate()
-{
-
-}
+double G1::calculate() { return 0; }
 
 /* ----------------------------------------------------------------------
    setup for G4 symmetric function
-------------------------------------------------------------------------- */
+------------------------------------------------------------------------ */
+
+G4::G4(double r_cutoff, double cost, double eta, double zeta, double lamb):
+        SymmetricFunction(r_cutoff), cost(cost), eta(eta), zeta(zeta), lamb(lamb) {}
 
 double G4::function(double rij, double rik, double rjk)
 {
@@ -50,23 +58,43 @@ double G4::function(double rij, double rik, double rjk)
     return res * fn_cutoff(rij) * fn_cutoff(rjk) * fn_cutoff(rik);
 }
 
-double G4::calculate()
-{
-
-}
+double G4::calculate() { return 0; }
 
 
 /* ----------------------------------------------------------------------
    setup for G5 symmetric function
 ------------------------------------------------------------------------- */
 
+G5::G5(double r_cutoff, double cost, double eta, double zeta, double lamb): SymmetricFunction(r_cutoff),
+                                                                        cost(cost), eta(eta),
+                                                                        zeta(zeta), lamb(lamb) {};
 double G5::function(double rij, double rik, double rjk)
 {
     double res =  pow(2.0, 1.0-zeta) * pow(1.0+lamb*cost, zeta) * exp( -eta * (rij*rij + rik*rik) );
     return res * fn_cutoff(rij) * fn_cutoff(rjk);
 }
 
-double G5::calculate()
-{
+double G5::calculate() { return 0; }
 
+/* ----------------------------------------------------------------------
+   setup for symmetric function
+------------------------------------------------------------------------- */
+
+SymmetricFunction *ACSF::make_SymmetricFunction(SymmetricFunctionType select)
+{
+    switch(select)
+    {
+        case SymmetricFunctionType::TG0 :
+            //return new G0();
+            break;
+        case SymmetricFunctionType::TG1 :
+            //return new G1();
+            break;
+        case SymmetricFunctionType::TG4 :
+            //return new G4();
+            break;
+        case SymmetricFunctionType::TG5 :
+            //return new G5();
+            break;
+    }
 }
--- symfn.h
+++ symfn.h
@@ -5,68 +5,76 @@
 #ifndef NNP_SYMMETRIC_FUNCTIONS_H
 #define NNP_SYMMETRIC_FUNCTIONS_H
 
+#include <vector>
+
+//namespace NNP_SF {
+
+typedef enum  {
+    TG0, TG1, TG4, TG5
+}  SymmetricFunctionType;
 
 
 class SymmetricFunction {
 
+protected:
+    virtual double function(double r);
+    virtual double calculate();
+
 public:
     double r_cutoff;
     double fn_cutoff(double r);
-    SymmetricFunction(double r_cutoff): r_cutoff(r_cutoff) {};
-    virtual double function(double r) {};
-    virtual double calculate() {};
-
+    SymmetricFunction(double r_cutoff);
 };
 
 
-class G0 : public SymmetricFunction {
-
+class G0 : SymmetricFunction {
 public:
-    G0(double r_cutoff): SymmetricFunction(r_cutoff) {};
-    double function(double r) { return fn_cutoff(r); };
-    double calculate();
-
-};
-
-
-class G1 : public SymmetricFunction {
-
-private:
-    double eta, rs;
-
-public:
-    G1(double r_cutoff, double eta, double rs): SymmetricFunction(r_cutoff), eta(eta), rs(rs) {};
+    G0(double r_cutoff);
     double function(double r);
     double calculate();
 };
 
 
-class G4 : public SymmetricFunction {
+class G1 : SymmetricFunction {
+private:
+    double eta, rs;
 
+public:
+    G1(double r_cutoff, double eta, double rs);
+    double function(double r);
+    double calculate();
+};
+
+
+class G4 : SymmetricFunction {
 private:
     double cost, eta, zeta, lamb;
 
 public:
-    G4(double r_cutoff, double cost, double eta, double zeta, double lamb): SymmetricFunction(r_cutoff),
-                                                                            cost(cost), eta(eta),
-                                                                            zeta(zeta), lamb(lamb) {};
+    G4(double r_cutoff, double cost, double eta, double zeta, double lamb);
     double function(double rij, double rik, double rjk);
     double calculate();
 };
 
 
-
-class G5 : public SymmetricFunction {
-
+class G5 : SymmetricFunction {
 private:
     double cost, eta, zeta, lamb;
 
 public:
-    G5(double r_cutoff, double cost, double eta, double zeta, double lamb): SymmetricFunction(r_cutoff),
-                                                                            cost(cost), eta(eta),
-                                                                            zeta(zeta), lamb(lamb) {};
+    G5(double r_cutoff, double cost, double eta, double zeta, double lamb);
     double function(double rij, double rik, double rjk);
     double calculate();
 };
+
+
+class ACSF {
+public:
+    /* factory method */
+    std::vector<SymmetricFunction *> list_of_symmetric_functions;
+    void add_symmetric_function(SymmetricFunctionType select);
+};
+
+//}
 
 #endif //NNP_SYMMETRIC_FUNCTIONS_H
