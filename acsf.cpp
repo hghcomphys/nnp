ca78fda014da8ef85df52055851639c2c929f3ff
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2019-06-04 15:44:27 +0200

    split two and three body symmetric function, new define for ACSF

--- acsf.cpp
+++ acsf.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "acsf.h"
+#include "atoms.h"
 #include <cmath>
 
 //using namespace NNP_SF;
@@ -11,9 +12,9 @@
    setup for base symmetric function base
 ------------------------------------------------------------------------- */
 
-ACSF::ACSF(double cutoff_radius): cutoff_radius(cutoff_radius) {}
+SymmetricFunction::SymmetricFunction(double cutoff_radius): cutoff_radius(cutoff_radius) {}
 
-double ACSF::cutoff_function(double r) {
+double SymmetricFunction::cutoff_function(double r) {
     return ( cos(M_PI*r/cutoff_radius) + 1.0 ) * 0.5;
 }
 
@@ -21,37 +22,28 @@ double ACSF::cutoff_function(double r) {
    setup for G0 symmetric function
 ------------------------------------------------------------------------- */
 
-G0::G0(std::vector<double> p): ACSF(p[0]) {}
+G0::G0(std::vector<double> p): TwoBodySymmetricFunction(p[0]) {}
 
 double G0::function(double rij) {
     return cutoff_function(rij);
 }
 
-double G0::function(double rij, double rik, double jk) { return 0; }
-
-double G0::calculate() {}
-
 /* ----------------------------------------------------------------------
    setup for G1 symmetric function
 ------------------------------------------------------------------------- */
 
-G1::G1(std::vector<double> p): ACSF(p[0]), eta(p[1]), rs(p[2]) {}
+G1::G1(std::vector<double> p): TwoBodySymmetricFunction(p[0]), eta(p[1]), rs(p[2]) {}
 
 double G1::function(double rij) {
     return exp( -eta * (rij-rs) * (rij-rs) ) * cutoff_function(rij);
 }
 
-double G1::function(double rij, double rik, double jk) { return 0; }
-
-double G1::calculate() {}
 
 /* ----------------------------------------------------------------------
    setup for G4 symmetric function
 ------------------------------------------------------------------------ */
 
-G4::G4(std::vector<double> p): ACSF(p[0]), cost(p[1]), eta(p[2]), zeta(p[3]), lamb(p[4]) {}
-
-double G4::function(double rij) { return 0; }
+G4::G4(std::vector<double> p): ThreeBodySymmetricFunction(p[0]), cost(p[1]), eta(p[2]), zeta(p[3]), lamb(p[4]) {}
 
 double G4::function(double rij, double rik, double rjk)
 {
@@ -59,16 +51,11 @@ double G4::function(double rij, double rik, double rjk)
     return res * cutoff_function(rij) * cutoff_function(rjk) * cutoff_function(rik);
 }
 
-double G4::calculate() {}
-
-
 /* ----------------------------------------------------------------------
    setup for G5 symmetric function
 ------------------------------------------------------------------------- */
 
-G5::G5(std::vector<double> p): ACSF(p[0]), cost(p[1]), eta(p[2]), zeta(p[3]), lamb(p[4]) {}
-
-double G5::function(double rij) { return 0; }
+G5::G5(std::vector<double> p): ThreeBodySymmetricFunction(p[0]), cost(p[1]), eta(p[2]), zeta(p[3]), lamb(p[4]) {}
 
 double G5::function(double rij, double rik, double rjk)
 {
@@ -76,4 +63,41 @@ double G5::function(double rij, double rik, double rjk)
     return res * cutoff_function(rij) * cutoff_function(rjk);
 }
 
-double G5::calculate() {}
+/* ----------------------------------------------------------------------
+   setup for ACSF
+------------------------------------------------------------------------- */
+ACSF::ACSF() {}
+
+ACSF::~ACSF() {
+
+    /* free the allocated memory for two-body symmetric fucntion*/
+    for (auto *each: two_body_symmetric_functions)
+        delete each;
+    two_body_symmetric_functions.clear();
+
+    /* free the allocated memory for three-body symmetric fucntion*/
+    for (auto *each: three_body_symmetric_functions)
+        delete each;
+    three_body_symmetric_functions.clear();
+}
+
+void ACSF::addTwoBodySymmetricFunction(TwoBodySymmetricFunction *symmetric_function) {
+    two_body_symmetric_functions.push_back(symmetric_function);
+}
+
+void ACSF::addThreeBodySymmetricFunction(ThreeBodySymmetricFunction *symmetric_function) {
+    three_body_symmetric_functions.push_back(symmetric_function);
+}
+
+double ACSF::calculate(AtomicConfiguration &configuration) 
+{
+    double res = 0.0;
+    for(auto &atom_i: configuration.atoms) {
+      for(auto &atom_j: configuration.atoms) {
+          if (atom_i.index == atom_j.index) continue;
+          double rij = configuration.distance(atom_i, atom_j);
+          res += two_body_symmetric_functions[0]->function(rij);
+      }
+    }
+    return res;
+}
--- acsf.h
+++ acsf.h
@@ -6,6 +6,7 @@
 #define NNP_ACSF_H
 
 #include <vector>
+#include "atoms.h"
 
 //namespace NNP_SF {
 
@@ -14,58 +15,72 @@
 //}  SymmetricFunctionType;
 
 
-class ACSF {
+class SymmetricFunction {
 public:
     double cutoff_radius;
     double cutoff_function(double r);
-    ACSF(double cutoff_radius);
-    virtual double function(double rij) {}; /*two-body*/
-    virtual double function(double rij, double rik, double jk) {}; /*three-body*/
-    virtual double calculate() {};
+    SymmetricFunction(double cutoff_radius);
+};
+
+/*Two-body*/
+class TwoBodySymmetricFunction: public SymmetricFunction { 
+public:
+    TwoBodySymmetricFunction(double cutoff_radius): SymmetricFunction(cutoff_radius) {};
+    virtual double function(double rij) {}; 
+};
+
+ /*Three-body*/
+class ThreeBodySymmetricFunction: public SymmetricFunction {
+public:
+    ThreeBodySymmetricFunction(double cutoff_radius): SymmetricFunction(cutoff_radius) {};
+    virtual double function(double rij, double rik, double jk) {}; 
 };
 
 
-class G0 : public ACSF {
+class G0 : public TwoBodySymmetricFunction {
 public:
     G0(std::vector<double> p);
     double function(double rij);
-    double function(double rij, double rik, double jk);
-    double calculate();
 };
 
 
-class G1 : public ACSF {
+class G1 : public TwoBodySymmetricFunction {
 private:
     double eta, rs;
 public:
     G1(std::vector<double> p);
     double function(double rij);
-    double function(double rij, double rik, double jk);
-    double calculate();
 };
 
 
-class G4 : public ACSF {
+class G4 : public ThreeBodySymmetricFunction {
 private:
     double cost, eta, zeta, lamb;
 public:
     G4(std::vector<double> p);
-    double function(double rij);
     double function(double rij, double rik, double rjk);
-    double calculate();
 };
 
 
-class G5 : public ACSF {
+class G5 : public ThreeBodySymmetricFunction {
 private:
     double cost, eta, zeta, lamb;
 public:
     G5(std::vector<double> p);
-    double function(double rij);
     double function(double rij, double rik, double rjk);
-    double calculate();
 };
 
+
+class ACSF {
+public:
+    std::vector<TwoBodySymmetricFunction *> two_body_symmetric_functions; /* factory method */
+    std::vector<ThreeBodySymmetricFunction *> three_body_symmetric_functions; /* factory method */
+    ACSF();
+    ~ACSF();
+    void addTwoBodySymmetricFunction(TwoBodySymmetricFunction *symmetric_function); /*add two-body symmettic function*/
+    void addThreeBodySymmetricFunction(ThreeBodySymmetricFunction *symmetric_function); /*add three-body symmettic function*/
+    double calculate(AtomicConfiguration &configuration);
+};
 
 //}
 
