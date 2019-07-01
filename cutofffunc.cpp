88b6f03fe1d56f841622e7d941e047988c5dd4a9
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2019-07-01 15:52:38 +0200

    add tanh cutoff function

--- cutofffunc.cpp
+++ cutofffunc.cpp
@@ -12,11 +12,18 @@ CutoffFunction::CutoffFunction() {}
 
 void CutoffFunction::setCutoffRadius(double cutoffRadius) {
     rc = cutoffRadius;
-    inv_rc = 1.0/cutoffRadius;
+    inv_rc = 1.0 / cutoffRadius;
 }
 
 // TODO: other types of cutoff function
-double CutoffFunction::fc(double r) {
+double CutoffFunction::fc(double r) 
+{
     if ( r > rc ) return 0;
-    return ( cos(M_PI*r*inv_rc) + 1.0 ) * 0.5;
+    
+    // COS TYPE
+    // return ( cos(M_PI*r*inv_rc) + 1.0 ) * 0.5; 
+    
+    // TANH TYPE
+    double const tanh1 = tanh(1.0 - r * inv_rc);
+    return tanh1 * tanh1 * tanh1;
 }
