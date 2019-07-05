601c695ea144cb945c8486e33dc913ca168f28fe
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2019-07-05 17:45:57 +0200

    use OpenNN within NeuralNetwok constructor

--- neuralnetwork.cpp
+++ neuralnetwork.cpp
@@ -9,28 +9,41 @@
 #include <iostream>
 using namespace std;
 
-#include "opennn.h"
-// using namespace OpenNN;
-
 /* ----------------------------------------------------------------------
    setup for class NeuralNetwork 
 ------------------------------------------------------------------------- */
 
-NeuralNetwork::NeuralNetwork() {
+NeuralNetwork::NeuralNetwork(const int& numberOfInputs, const std::vector<int>& hiddenLayersSize)
+{
+    const int numberOfHiddenLayers = hiddenLayersSize.size();
+    OpenNN::Vector<int> layersSize(numberOfHiddenLayers+2);
+    layersSize[0] = numberOfInputs;  //input layer
+    for (int i=0; i<numberOfHiddenLayers; i++)
+            layersSize[i+1] = hiddenLayersSize[i];
+    layersSize[numberOfHiddenLayers+1] = 1;  //Output layer
 
-        OpenNN::MultilayerPerceptron mlp(1, 3, 1);
-        cout << "number of inputs: " <<mlp.get_inputs_number() << endl;
-        cout << "number of outputs: " <<mlp.get_outputs_number() << endl;
-        cout << mlp.get_outputs_number() << endl;
+    for(int i=0; i< layersSize.size(); i++)
+        cout << layersSize[i] << " ";
+    cout << endl;
 
-        OpenNN::Vector<double> input(1);
-        input[0] = 0.5;
-        OpenNN::Vector<double> output = mlp.calculate_outputs( input );
-        cout << "output = " << output << endl;
+    OpenNN::MultilayerPerceptron mlp(layersSize);
+    cout << mlp.get_inputs_number() << endl;
+    cout << mlp.get_layers_number() << endl;  //hidden layers + output layer 
+    cout << mlp.get_outputs_number() << endl;
 
-        OpenNN::Matrix<double> jacobian = mlp.calculate_Jacobian ( input );
-        cout << "jacobian = " << jacobian << endl;
+    OpenNN::Vector<double> input(1);
+    input[0] = 0.5;
+    OpenNN::Vector<double> output = mlp.calculate_outputs( input );
+    cout << "output = " << output << endl;
 
+    OpenNN::Matrix<double> jacobian = mlp.calculate_Jacobian ( input );
+    cout << "jacobian = " << jacobian << endl;
 }
 
 NeuralNetwork::~NeuralNetwork() {}
+
+// int NeuralNetwork::getNumberOfInputs() const { return numberOfInputs; }
+
+// int NeuralNetwork::getNumberOfOutputs() const { return numberOfOutputs; }
+
+// const std::vector<int>& NeuralNetwork::getNumberOfHiddenLayers() const { return numberOfHiddenLayers; }
--- neuralnetwork.h
+++ neuralnetwork.h
@@ -6,16 +6,21 @@
 #define NNP_NEURALNETWORK_H
 
 #include <vector>
-#include "acsf.h"
+#include "opennn.h"
+// using namespace OpenNN;
 
 
 class NeuralNetwork {
 public:
     NeuralNetwork();
+    NeuralNetwork::NeuralNetwork(const int& numberOfInputs, const std::vector<int>& hiddenLayersSize);
     ~NeuralNetwork();
+    // int getNumberOfInputs() const;
+    // int getNumberOfOutputs() const;
+    // const std::vector<int>& getNumberOfHiddenLayers() const;
 
 private:
-
+    OpenNN::MultilayerPerceptron neuralNetwork;
 };
 
 
