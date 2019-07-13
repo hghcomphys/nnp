3590b06fce50b7e9c4a09f40345374f3cf9fee77
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2019-07-13 13:31:52 +0200

    remove unnecessary comments

--- acsf.cpp
+++ acsf.cpp
@@ -7,6 +7,9 @@
 #include "atoms.h"
 #include <iostream>
 
+#include <sstream>
+#include <fstream>
+
 /* ----------------------------------------------------------------------
    setup for ACSF
 ------------------------------------------------------------------------- */
@@ -66,75 +69,6 @@ std::vector<std::vector<double>> ACSF::calculateSF(Atoms &configuration)
         values.push_back( calculateSF(configuration, atomIndex) );
 
     return values;
-
-
-    // // TODO: optimization
-    // const int n_2b = listOfTwoBodySF.size();
-    // const int n_3b = listOfThreeBodySF.size();
-    // const int nrow =  configuration.getNumberOfAtomsForElement(centralElement);
-    // const int ncol =  n_2b + n_3b;
-
-    // values.resize(nrow, std::vector<double>(ncol));
-    // for (auto& col: values) // initialize to zero
-    //     std::fill(col.begin(), col.end(), 0.0);
-
-    // int elementIndex = 0;
-    // auto atoms = configuration.getListOfAtoms();
-    // auto listOfIndexForCentralElement = configuration.getListOfIndexForElement(centralElement);
-    // for(int i: listOfIndexForCentralElement) 
-    // {
-    //     // central element
-    //     Atom& atom_i = atoms[i];
-
-    //     // symmetry function values for each atom with central element type
-    //     auto& elementValues = values[elementIndex++];
-
-    //     // Loop over all two-body symmetry functions
-    //     for (int n=0; n<n_2b; n++) 
-    //     {
-    //         for(int j: configuration.getListOfIndexForElement(listOfTwoBodyNeighborElement[n])) {
-    //                 Atom& atom_j = atoms[j];
-    //                 if (atom_j.getIndex() == atom_i.getIndex()) continue;
-    //                 const double rij = configuration.distance(atom_i, atom_j);
-    //                 elementValues[listOfTwoBodySFindex[n]] += listOfTwoBodySF[n]->function(rij);
-    //             } 
-    //     }
-
-    //     // Loop over all tree-body symmetry functions
-    //     for (int n=0; n<n_3b; n++) 
-    //     {
-    //         // first neighbors
-    //         for(int j: configuration.getListOfIndexForElement(listOfThreeBodyNeighborElement1[n])) {
-                    
-    //                 Atom& atom_j = atoms[j];
-    //                 if (atom_j.getIndex() == atom_i.getIndex()) continue;
-                    
-    //                 double drij[3];
-    //                 const double rij = configuration.distance(atom_i, atom_j, drij);
-
-    //                 // second neighbors
-    //                 for(int k: configuration.getListOfIndexForElement(listOfThreeBodyNeighborElement2[n])) {
-                        
-    //                     Atom& atom_k = atoms[k];
-    //                     if (atom_k.getIndex() == atom_i.getIndex()) continue;
-
-    //                     double drik[3];
-    //                     const double rik = configuration.distance(atom_i, atom_k, drik);
-    //                     const double rjk = configuration.distance(atom_j, atom_k);
-
-    //                     // cosine of angle between k--<i>--j atoms
-    //                     double cost = 0;
-    //                     for (int d=0; d<3; d++)
-    //                         cost += drij[d] * drik[d];
-    //                     const double inv_r = 1.0 / rij / rik;
-    //                     cost *= inv_r;
-    //                     // std::cout << cost << std::endl;
-
-    //                     elementValues[listOfThreeBodySFindex[n]] += listOfThreeBodySF[n]->function(rij, rik, rjk, cost);
-    //                 }
-    //         }
-    //     }
-    // }
 }
 
 std::vector<double> ACSF::calculateSF(Atoms &configuration, int atomIndex)
