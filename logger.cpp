ac7074ca18b28af0a7509f3516dc53186047eef8
Hossein <hgh.comphys@gmail.com>
2019-08-13 15:11:20 +0200

    add buffer stream to LOG

--- logger.cpp
+++ logger.cpp
@@ -2,45 +2,61 @@
 
 #include "logger.h"
 #include <iostream>
+#include <stdexcept>
 
 /* ----------------------------------------------------------------------
    setup for class Logger  
 ------------------------------------------------------------------------- */
 
-LOG::LOG() {}
+LOG::LOG(): LOG(INFO) {}
 
-LOG::LOG(LOG_t type) 
+LOG::LOG(LOG_t level):  isOpened(false), messageLevel(DEBUG)
 {
-    msglevel = type;
-    if(false) {
-        operator << ("["+getLabel(type)+"]");
+    messageLevel = level;
+    if( isHeader ) {
+        operator << ("["+getLabel(level)+"] ");
     }
 }
 
-LOG::~LOG() {
-    if(opened) {
-        std::cout << std::endl;
+LOG::~LOG() 
+{
+    if(isOpened) {
+
+        buffer << "\n";
+        switch ( messageLevel )
+        {
+        case ERROR:
+            std::cerr << buffer.str();
+            break;
+
+        default:
+            std::cout << buffer.str();
+        }      
     }
-    opened = false;
+    isOpened = false;
 }
 
-LOG& LOG::operator<< (const std::string& msg) 
+LOG& LOG::operator<< (const std::string& message) 
 {
-    if( msglevel >= verbosity ) {
-        std::cout << msg;
-        opened = true;
+    if( messageLevel >= verbosity ) {
+        buffer << message;
+        isOpened = true;
     }
     return *this;
 }
 
-inline std::string getLabel(LOG_t type) 
+inline std::string LOG::getLabel(LOG_t level) 
 {
     std::string label;
-    switch(type) {
+    switch(level) {
         case DEBUG: label = "DEBUG"; break;
         case INFO:  label = "INFO "; break;
         case WARN:  label = "WARN "; break;
         case ERROR: label = "ERROR"; break;
     }
     return label;
+}
+
+const std::string LOG::toString() const {
+    return buffer.str();
 }
