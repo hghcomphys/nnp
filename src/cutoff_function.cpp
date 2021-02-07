524fe0d0289eb34feb425a1afb03f78130e0a8a7
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2021-02-07 10:10:17 +0100

    removed inline methods in cutoff function

--- src/cutoff_function.cpp
+++ src/cutoff_function.cpp
@@ -28,8 +28,37 @@
 ------------------------------------------------------------------------- */
 CutoffFunction::CutoffFunction() {}
 
-void CutoffFunction::setCutoffRadius(double cutoffRadius) 
+void CutoffFunction::setCutoffRadius(double rc) 
 {
-    rc = cutoffRadius;
-    inv_rc = 1.0 / cutoffRadius;
+    rc = rc;
+    inv_rc = 1.0 / rc;
+}
+
+// TODO: other types of cutoff function
+// TODO: using inline can reduce computational time
+double CutoffFunction::fc(double r) 
+{
+    if ( r > rc ) return 0;
+    
+    // COS TYPE
+    // return ( cos(M_PI * r * inv_rc) + 1.0 ) * 0.5; 
+    
+    // TANH TYPE
+    double const tmp = tanh(1.0 - r * inv_rc);
+    return tmp * tmp * tmp;
+}
+
+// TODO: other types of cutoff function
+// TODO: using inline can reduce computational time
+double CutoffFunction::dfc(double r) 
+{
+    if ( r > rc ) return 0;
+    
+    // COS TYPE
+    // return -M_PI_2 * inv_rc * sin(M_PI * r * inv_rc);
+    
+    // TANH TYPE
+    double tmp = tanh(1.0 - r * inv_rc);
+    tmp *= tmp;
+    return 3.0 * tmp * (tmp - 1.0) * inv_rc;
 }
